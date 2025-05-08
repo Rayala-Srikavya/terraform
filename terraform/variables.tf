3f4f2d88e050d4d3ade7daa57205a66bc0fdd034
Rayala-Srikavya <142518414+Rayala-Srikavya@users.noreply.github.com>
2025-05-08 15:36:10 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,6 @@
 variable "snowflake_account" {}
-variable "snowflake_organization_name" {}
 variable "snowflake_user" {}
 variable "snowflake_password" {
   sensitive = true
 }
-variable "target_database" {}
-variable "target_schema" {}
+
