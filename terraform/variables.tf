6a46cfa51eee4d8fc01d26c41011f992e16943d8
Rayala-Srikavya <142518414+Rayala-Srikavya@users.noreply.github.com>
2025-05-08 15:54:34 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,3 +4,19 @@ variable "snowflake_password" {
   sensitive = true
 }
 
+variable "database" {
+  description = "Name of the Snowflake database to create"
+  type        = string
+}
+
+variable "schema" {
+  description = "Name of the schema to create"
+  type        = string
+}
+
+variable "table_name" {
+  description = "Name of the table to create (used in SnowSQL step)"
+  type        = string
+}
+
+
