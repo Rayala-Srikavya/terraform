c0aafaaa2210dbcecacf926e33595cd64284c748
Rayala-Srikavya <142518414+Rayala-Srikavya@users.noreply.github.com>
2025-05-08 16:42:14 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,9 +12,9 @@ terraform {
 }
 
 provider "snowflake" {
-  account  = var.snowflake_account
-  user     = var.snowflake_user
-  password = var.snowflake_password
+  account_id = var.snowflake_account
+  user       = var.snowflake_user
+  password   = var.snowflake_password
 }
 
 resource "snowflake_database" "user_db" {
