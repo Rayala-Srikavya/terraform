568266f10f2ca2efe282bf0ba965d8bc0c3edb34
Rayala-Srikavya <142518414+Rayala-Srikavya@users.noreply.github.com>
2025-05-12 11:45:06 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,22 +25,62 @@ resource "snowflake_schema" "user_schema" {
 }
 
 resource "snowflake_table" "user_table" {
-  name     = var.table_name
-  database = snowflake_database.user_db.name
-  schema   = snowflake_schema.user_schema.name
+  name     = var.table_name
+  database = snowflake_database.user_db.name
+  schema   = snowflake_schema.user_schema.name
 
-  column {
-    name = "id"
-    type = "NUMBER"
-  }
+  column {
+    name = "id"
+    type = "NUMBER"
+  }
 
-  column {
-    name = "name"
-    type = "VARCHAR"
-  }
+  column {
+    name = "name"
+    type = "VARCHAR"
+  }
 
-  column {
-    name = "created_at"
-    type = "TIMESTAMP_LTZ"
-  }
+  column {
+    name = "created_at"
+    type = "TIMESTAMP_LTZ"
+  }
+}
+
+variable "SNOWFLAKE_ACCOUNT" {
+  description = "Snowflake account name"
+  type        = string
+}
+
+variable "snowflake_organization_name" {
+  description = "Snowflake organization name"
+  type        = string
+}
+
+variable "host" {
+  description = "Snowflake host"
+  type        = string
+}
+
+variable "SNOWFLAKE_USER" {
+  description = "Snowflake user name"
+  type        = string
+}
+
+variable "SNOWFLAKE_PASSWORD" {
+  description = "Snowflake user password"
+  type        = string
+}
+
+variable "database" {
+  description = "Name of the Snowflake database to create"
+  type        = string
+}
+
+variable "schema" {
+  description = "Name of the schema to create"
+  type        = string
+}
+
+variable "table_name" {
+  description = "Name of the table to create"
+  type        = string
 }
